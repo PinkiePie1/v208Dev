6cd159087d842e41d94351ee63685fe8862eac0a
Harchimemedes <474890266@qq.com>
2025-04-18 18:32:29 +0800

    初始化全部定时器

--- examples/nixie/main.c
+++ examples/nixie/main.c
@@ -13,9 +13,10 @@ void TIM_Init(void)
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure={0};
 
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA | RCC_APB2Periph_TIM1, ENABLE ); //开启GPIOA和TIM1的外设时钟
+	RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM2 | RCC_APB1Periph_TIM3, ENABLE );//开启TIM2和3的外设时钟，他们挂在APB1上。
 
 	//设置PA8为复用输出
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8|GPIO_Pin_6|GPIO_Pin_0;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init( GPIOA, &GPIO_InitStructure );
@@ -27,6 +28,9 @@ void TIM_Init(void)
 	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseInit( TIM1, &TIM_TimeBaseInitStructure);
 
+	TIM_TimeBaseInit( TIM2, &TIM_TimeBaseInitStructure);
+	TIM_TimeBaseInit( TIM3, &TIM_TimeBaseInitStructure);
+
 	//输出设置，PWM模式2，50%占空比，高有效，只开CH1
 	//注意要开对应的channel就要调用OCxInit。
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
@@ -35,11 +39,23 @@ void TIM_Init(void)
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OC1Init( TIM1, &TIM_OCInitStructure );
 
+	TIM_OC1Init( TIM2, &TIM_OCInitStructure );//TIM2CH1,PA0
+	TIM_OC1Init( TIM3, &TIM_OCInitStructure );//TIM3CH1,PA6
+
 	
 	TIM_CtrlPWMOutputs(TIM1, ENABLE );
 	TIM_OC1PreloadConfig( TIM1, TIM_OCPreload_Disable );
 	TIM_ARRPreloadConfig( TIM1, ENABLE );
+	TIM_CtrlPWMOutputs(TIM2, ENABLE );
+	TIM_OC1PreloadConfig( TIM2, TIM_OCPreload_Disable );
+	TIM_ARRPreloadConfig( TIM2, ENABLE );
+	TIM_CtrlPWMOutputs(TIM3, ENABLE );
+	TIM_OC1PreloadConfig( TIM3, TIM_OCPreload_Disable );
+	TIM_ARRPreloadConfig( TIM3, ENABLE );
 	TIM_Cmd( TIM1, ENABLE );
+	TIM_Cmd( TIM2, ENABLE );
+	TIM_Cmd( TIM3, ENABLE );
+	
 }
 
 /*********************************************************************
@@ -61,6 +77,8 @@ int main(void)
     	duty+=5;
         Delay_Ms(10);
         TIM1->CH1CVR = duty<<1;
+        TIM2->CH1CVR = duty<<1;
+        TIM3->CH1CVR = duty<<1;
     }
 
 }
